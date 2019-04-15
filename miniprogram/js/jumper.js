1d53c17813256f66b5f1cb6d4e41ea6412859b63
哈斯普拉提 <1498893392@qq.com>
2019-04-15 19:59:17 +0800

    Debug the main bug and create the jumper.

--- miniprogram/js/jumper.js
+++ miniprogram/js/jumper.js
@@ -1,14 +1,12 @@
 const context = canvas.getContext('2d')
 let image = wx.createImage()
-export default class TestMain {
+export default class Jumper {
   constructor(){
     console.log(canvas.width, canvas.height)
     image.src = 'images/test.jpg'
     image.onload = function(){
-        context.drawImage(image,0,0)
+        context.drawImage(image,0,0,300,300,0,0,100,100)
         console.log('ok!',image.width, image.height)
     }
-    // context.fillStyle = 'red'
-    // context.fillRect(0,0,100,100);
   }
 }
